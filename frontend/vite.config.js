8ea1d3b2bd2c97c0b78b49bd78a15275558c377c
Uphill001 <121593876+Uphill001@users.noreply.github.com>
2024-06-22 20:39:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://mern-auth-ivory.vercel.app/',
+        target: 'https://mern-auth-ivory.vercel.app',
         changeOrigin: true,
       },
     },
